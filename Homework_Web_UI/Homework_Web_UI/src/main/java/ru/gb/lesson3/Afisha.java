486b7444212b1abaf3404aed60d83742bd76d6f3
Olga-Lopatina <71078294+Olga-Lopatina@users.noreply.github.com>
2022-09-20 18:22:58 +0300

    Add files via upload

---
+++ Homework_Web_UI/Homework_Web_UI/src/main/java/ru/gb/lesson3/Afisha.java
@@ -0,0 +1,23 @@
+package ru.gb.lesson3;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Afisha {
+    public static void main (String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+
+        WebDriver webDriver = new ChromeDriver();
+        webDriver.get("https://afisha.ru");
+
+        WebElement movie = webDriver.findElement(By.xpath("a[href=/movie/]"));
+        WebElement changeWorld = webDriver.findElement(By.xpath("//button[text()='Изменившие мир']"));
+
+        WebDriver.quit();
+    }
+
+}
---
+++ Homework_Web_UI/Homework_Web_UI/src/main/java/ru/gb/lesson3/SeleniumStart.java
@@ -0,0 +1,18 @@
+package ru.gb.lesson3;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumStart {
+    public static void main(String[] args)
+    {
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://google.com");
+
+        //Thread.sleep(5000);
+        driver.quit();
+    }
+
+}
